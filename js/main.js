2866a1741981a867a8fd88c050ca787ac62e58be
l2bsolucoes <contato@l2bsolucoes.com.br>
2020-11-08 01:08:45 -0300

    Estudando array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-var nome = "Leslie Bagalho";
-var idade = 40;
-var idade2 = 10;
+//var nome = "Leslie Bagalho";
+//var idade = 40;
+//var idade2 = 10;
 
 //alert (nome + " tem " + idade + " anos")
 
-console.log(nome);
-console.log(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - ")); //acrescenta o caracter entre os nomes da lista
+
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
