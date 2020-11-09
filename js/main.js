d4b4f446b959efd8b241fe051e28cd3e85fcb615
l2bsolucoes <contato@l2bsolucoes.com.br>
2020-11-09 00:31:29 -0300

    Usando function

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-//var nome = "Leslie Bagalho";
-//var idade = 40;
-//var idade2 = 10;
+var validar = 0;
 
-//alert (nome + " tem " + idade + " anos")
+function validaIdade(idade){
+    if(idade = 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
 
-//console.log(nome);
-//console.log(idade + idade2);
-
-//var lista = ["maça", "pera", "laranja"];
-//lista.push("uva");
-//lista.pop();
-
-//console.log(lista);
-//console.log(lista.toString());
-//console.log(lista.join(" - ")); //acrescenta o caracter entre os nomes da lista
-
-/*var fruta = {nome:"maça", cor:"vermelha"}
-console.log(fruta.nome);
-alert(fruta.cor);*/
-
-var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
-console.log(frutas);
-alert(frutas[1].nome);
+var idade = prompt("Qual sua idade");
+validaIdade(idade)
+console.log(validar);
