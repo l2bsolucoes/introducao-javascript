b6f1a4ce2544d965ec832b8620665c1f4246f648
l2bsolucoes <contato@l2bsolucoes.com.br>
2020-11-11 02:12:24 -0300

    finalizando estudos

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ let A = parseInt(line[0]);
 let B = parseInt(line[1]);
 let total = A + B; // Altere o valor da variável com o cálculo esperado
 console.log("X = " + total);
+
+
+/*
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let total = valor1 * valor2; // Altere o valor da variável com o cálculo esperado 
+console.log("PROD = " + total);*/
+
+/*
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseFloat(gets());
+let salary = parseFloat(valor2 * valor3).toFixed(2); // Digite aqui o calculo do salário
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);*/
